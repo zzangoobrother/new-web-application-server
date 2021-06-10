3760847d4c3a0788bbd07b6abaef92b31c68af3c
zzangoobrother <com1368@naver.com>
2021-06-11 00:04:20 +0900

    GET 방식으로 회원가입 및 url로 입력값 학습 POST 방식으로 회원가입 및 HTTP BODY에서 입력값 받기 리다이렉트 하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,13 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,21 +36,42 @@ public class RequestHandler extends Thread {
             }
 
             String[] tokens = line.split(" ");
-
+            int bodyLength = 0;
             while (!line.equals("")) {
                 line = br.readLine();
                 log.debug("request header : {}", line);
+                if (line.startsWith("Content-Length")) {
+                    bodyLength = getContentLength(line);
+                }
             }
 
-            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+            String url = tokens[1];
+            if (url.startsWith("/user/create")) {
+                // get 방식
+//                int index = url.indexOf("?");
+//                String queryString = url.substring(index + 1);
 
-            DataOutputStream dos = new DataOutputStream(out);
-            //byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+                String queryString = IOUtils.readData(br, bodyLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("user : {}", user);
+
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos, "/index.html");
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private int getContentLength(String line) {
+        return Integer.parseInt(line.split(":")[1].trim());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -54,6 +79,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String url) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + url + " \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
